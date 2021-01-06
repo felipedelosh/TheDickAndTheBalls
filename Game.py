017a2344fe61c4722b07f08d8adc6e6ef7d2ee12
Andres Felipe Herndandez <38327255+felipedelosh@users.noreply.github.com>
2021-01-06 11:16:34 -0500

    Creating a Main Machine of states
    Try to run

--- Game.py
+++ Game.py
@@ -10,7 +10,21 @@ from MachineOfStates import *
 class Game:
     def __init__(self):
         self.gameStatus = MachineOfStates() # say a game status main, gameOPT, 1pvsCp, 1pvs2p
+        self.configureGameStatus()
         self.Players =[]
+        
 
     def configureGameStatus(self):
-        self.gameStatus
+        self.gameStatus.addNode("main")
+        self.gameStatus.addNode("game")
+        self.gameStatus.addNode("options")
+        self.gameStatus.addNode("pause")
+        self.gameStatus.setInitialPointer("main")
+        self.gameStatus.addConection("main", "game", 0)
+        self.gameStatus.addConection("main", "options", 1)
+        self.gameStatus.addConection("game", "game", 1)
+        self.gameStatus.addConection("game", "pause", 0)
+        self.gameStatus.addConection("pause", "game", 0)
+        self.gameStatus.addConection("pause", "options", 1)
+        self.gameStatus.addConection("options", "game", 0)
+        self.gameStatus.addConection("options", "main", 1)
--- main.py
+++ main.py
@@ -11,20 +11,25 @@ __author__      = "FelipedelosH"
 
 from controller import *
 from tkinter import *
+import os # To know path project
+import sys # To close APP
 
 class LunarLoko:
     def __init__(self):
+        self.pathProject = str(os.path.dirname(os.path.abspath(__file__)))
         self.controller = Controller()
         self.display = Tk() # Show a display
         self.screem = Canvas(self.display, bg="snow") # paint all things
-
-        
-
+        # Imagenes to background
+        self.imgBgMain = PhotoImage(file=self.pathProject+"\\RESOURCES\\IMG\\main.gif")
+        # Buttons to controller a main title
+        self.btnMainPlayGame = Button(self.screem, text="Play Game")
+        self.btnMainOptions = Button(self.screem, text="Options")
+        self.btnMainExit = Button(self.screem, text="Exit", command=self.closeProgram)
         # Vars to configure a size of display
         self.resolutionW = 720
         self.resolutionH = 480 
         
-
         self.showDisplay()
 
     def showDisplay(self):
@@ -32,16 +37,20 @@ class LunarLoko:
         self.configureSizeOfDisplay()
         self.screem.place(x=0, y=0)
         
-
         self.display.after(0, self.refreshDisplay())
         self.display.mainloop()
 
     def refreshDisplay(self):
         """
         Refresh a screem every 30 ms
+        show a interface from controller.game.gameStatus
         """
-        
 
+        if self.controller.game.gameStatus.pointer == "main":
+            self.screem.create_image(0, 0, image=self.imgBgMain, anchor=NW)
+            self.btnMainPlayGame.place(x=self.resolutionW*0.7-(self.resolutionW*0.1), y=self.resolutionH/3)
+            self.btnMainOptions.place(x=self.resolutionW*0.7-(self.resolutionW*0.1), y=self.resolutionH/3 + self.resolutionH*0.1)
+            self.btnMainExit.place(x=self.resolutionW*0.7-(self.resolutionW*0.1), y=self.resolutionH/3 + self.resolutionH*0.2)
 
         self.screem.after(30, self.refreshDisplay)
 
@@ -50,6 +59,19 @@ class LunarLoko:
         self.display.geometry(resolution)
         self.screem['width'] = self.resolutionW
         self.screem['height'] = self.resolutionH
+
+    def configureGameStatus(self, key):
+        """
+        Modify a main machine of states:
+        The main sructure (Machine of states) to change Main Menu, Pause, Game, Options
+        """
+        self.controller.game.gameStatus.insertSimbol(key)
+
+    def closeProgram(self):
+        """
+        End the program and close
+        """
+        sys.exit()
 
 
 
